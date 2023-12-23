afc631d9f05a7639677ab9633af2356221456a8f
unknown <you@example.com>
2023-12-23 12:40:30 -0300

    Configurando o Serviço

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ClienteService {
 
-  constructor() { }
+  // Url da API
+  private url:string = 'http://localhost:8080';
+
+  // Responsável pelas requisições da API
+  constructor(private http:HttpClient) { }
 }
+ 
